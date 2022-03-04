b2140b812202740405001ca69372c40735ea29d3
VHM Infotecch <89240446+vhminfotech@users.noreply.github.com>
2022-03-04 18:45:20 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,31 +9,6 @@
  * @since Twenty Twenty 1.0
  */
 
-/**
- * Table of Contents:
- * Theme Support
- * Required Files
- * Register Styles
- * Register Scripts
- * Register Menus
- * Custom Logo
- * WP Body Open
- * Register Sidebars
- * Enqueue Block Editor Assets
- * Enqueue Classic Editor Styles
- * Block Editor Settings
- */
-
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- *
- * @since Twenty Twenty 1.0
- */
-
 // vhm code
 
 //show custom field
